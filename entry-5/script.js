5eabad6f4c8ee81abd4b6af98447213e9da8e602
heq879 <heq879@newschool.edu>
2023-10-09 22:28:38 -0400

    entry 5 update

---
+++ entry-5/script.js
@@ -0,0 +1,19 @@
+//this js script is for the type box function
+
+function sendMessage() {
+    const messageInput = document.getElementById("message");
+    const message = messageInput.value;
+    const chatDisplay = document.getElementById("chatDisplay");
+    
+    if (message.trim() !== "") {
+        // create a new paragraph element for the message
+        const messageElement = document.createElement("p");
+        messageElement.textContent = message;
+        
+        // append the message to the chat display
+        chatDisplay.appendChild(messageElement);
+        
+        // clear the input field
+        messageInput.value = "";
+    }
+}
